73f847bd6eb2dfd2fd72514e7372ef218ba599f0
gsmith23 <gary.smith@ed.ac.uk>
2021-02-01 23:28:50 +0000

    minor, temp

--- edit_histo.C
+++ edit_histo.C
@@ -1,21 +1,23 @@
 void edit_histo(double x_max = -1.){
   
+
   // The name of the file to input.
   // Ie the simulation output file name.
   char filename[128] = "EdMedPhysics.root";
 
   // Declare a TFile object to read in data from.
   // Use the filename character array from above.
-  TFile * input_file = new TFile(filename,"append");
-
+  //TFile * input_file = new TFile(filename,"update");
+  TFile * input_file = new TFile(filename,"");
+  
   // Connect to a histogram in the file.
   // https://root.cern.ch/doc/master/classTH1D-members.html
-  TH1D * Edep_vs_Z_edited = (TH1D*)input_file->Get("Edep_vs_z");  
+  TH1D * Edep_vs_Z_edited = (TH1D*)input_file->Get("Edep_vs_z")->Clone();  
   
   Edep_vs_Z_edited->SetName("Edep_vs_Z_edited");
 
   // Fix the histogram axis labels.
-  Edep_vs_Z_edited->SetTitle("Edep_vs_Z;Z in phantom (mm);Accumulated energy deposited (MeV)");
+  Edep_vs_Z_edited->SetTitle("Edep_vs_Z_edited;Z in phantom (mm);Accumulated energy deposited (MeV)");
   
   // Fix the x-axis range.
   // Stay in the while loop until a valid answer is given.
@@ -38,7 +40,7 @@ void edit_histo(double x_max = -1.){
   std::cout << std::endl;
   
   // Set the x-axis range to the value specified above.
-  Edep_vs_Z_edited->GetXaxis()->SetRangeUser(-5,x_max);
+  //Edep_vs_Z_edited->GetXaxis()->SetRangeUser(-5,x_max);
 
   // The stats box can be modified.
   // (https://root.cern.ch/doc/master/classTPaveStats.html)
@@ -46,7 +48,7 @@ void edit_histo(double x_max = -1.){
   // Draw stats box with: Entries and Integral only.
   // Entries is the number of hits in which some energy was deposited.
   // Intergal is total energy deposited in the phantom during the entire run.
-  gStyle->SetOptStat(1000010);
+  ////  gStyle->SetOptStat(1000010);
   
   // To not draw stats box,
   // uncomment the line below.
@@ -55,30 +57,34 @@ void edit_histo(double x_max = -1.){
   // Dont show the title. 
   // To include the title, 
   // comment in the line below by putting // at the start of the line.
-  gStyle->SetOptTitle(0);
+  //gStyle->SetOptTitle(0);
 
   // Create a canvas to draw on
   // and later to save as a pdf
-  TCanvas canvas;
+  //TCanvas canvas;
+  //  TCanvas * canvas = new TCanvas();
   // Draw histogram as a simple histogram 
   // with no error bars.
   // This will only apply to the pdf image
-  Edep_vs_Z_edited->Draw("hist");
+  //Edep_vs_Z_edited->Draw("hist");
   
   
   // Save the canvas as a pdf
-  canvas.SaveAs("my_histogram.pdf");
+  //canvas->SaveAs("my_histogram.pdf");
   // NB Several other file types can be specified
   // including image files (.png,.eps,.jpg); or even a .root or .C file.
   
   // Add the edited histogram to the existing root file.
-  input_file->Write();
+  //input_file->Write("",TObject::kOverwrite);
   
-   std::cout << std::endl;
-   std::cout << " " << filename << " has been updated" << std::endl;
-   std::cout << std::endl;
+  //canvas->Close();
 
-  input_file->Close();
+//   std::cout << std::endl;
+//   std::cout << " " << filename << " has been updated" << std::endl;
+//   std::cout << std::endl;
+  
+//  input_file->Write();
+  //input_file->Close();
   
   // quit root and return to the terminal command prompt
   gApplication->Terminate();
